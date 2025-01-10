23ad3119798239ee7e95c4aa840d58e2ddd609fb
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2025-01-10 14:40:52 -0300

    cambios nuevos

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React, { useState } from 'react';
 
+
 const FelizCumpleaños = () => {
  const carouselImages = [
-    'https://i.ibb.co/7kfQLTK/image1.jpg',
     'https://i.ibb.co/8jqMq9t/image2.jpg',
-    'https://i.ibb.co/yXhPRD7/image3.jpg',
-    'https://i.ibb.co/Fz037hP/image4.jpg',
-    'https://i.ibb.co/TMQGp7g/image5.jpg',
-    'https://i.ibb.co/Z2SYLKS/image6.jpg',
-    'https://i.ibb.co/7jfWpYP/image7.jpg',
     'https://i.ibb.co/rmpy7Qz/image8.jpg',
-    'https://i.ibb.co/n8cMCrF/image9.jpg',
     'https://i.ibb.co/b6Q1hX9/image10.jpg',
-    'https://i.ibb.co/NTfnQJF/image11.jpg',
+    'https://i.ibb.co/n8cMCrF/image9.jpg',
     'https://i.ibb.co/SBWB1bc/image12.jpg',
     'https://i.ibb.co/ZTTT582/image13.jpg',
+    'https://i.ibb.co/NTfnQJF/image11.jpg',
     'https://i.ibb.co/1s0kYD5/image14.jpg',
-    'https://i.ibb.co/dGfRg7J/image15.jpg',
+    'https://i.ibb.co/7jfWpYP/image7.jpg',
     'https://i.ibb.co/xs12vrj/image16.jpg',
+    'https://i.ibb.co/dGfRg7J/image15.jpg',
     'https://i.ibb.co/rFRYjVb/image17.jpg',
-    'https://i.ibb.co/R9pwFsH/image18.jpg',
     'https://i.ibb.co/N7FgpnX/image19.jpg',
+    'https://i.ibb.co/qCXXRmW/image23.jpg',
     'https://i.ibb.co/dcHV24g/image20.jpg',
     'https://i.ibb.co/fMNy655/image21.jpg',
-    'https://i.ibb.co/BgsbQW3/image22.jpg',
-    'https://i.ibb.co/qCXXRmW/image23.jpg',
-    'https://i.ibb.co/k4jrrHB/image24.jpg',
     'https://i.ibb.co/zf35j4V/image25.jpg',
+    'https://i.ibb.co/R9pwFsH/image18.jpg',
+    'https://i.ibb.co/BgsbQW3/image22.jpg',
     'https://i.ibb.co/ctwsnG4/image26.jpg',
+    'https://i.ibb.co/k4jrrHB/image24.jpg',
  ];
 
  const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -46,20 +42,21 @@ const FelizCumpleaños = () => {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  background: 'linear-gradient(to right, #FF0000, #FFFF00)',
-  color: '#FFFFFF', // Texto en color blanco para resaltar en el gradiente
+  background: 'linear-gradient(to right, violet, #FFFF00)',
+  color: 'grey', // Texto en color blanco para resaltar en el gradiente
   fontSize: '24px',
   fontWeight: 'bold',
   textAlign: 'center',
 };
 
-
- const carouselStyle = {
-    position: 'relative',
-    width: '100%',
-    height: '70vh', // 70% height of viewport
-    overflow: 'hidden',
- };
+const carouselStyle = {
+  position: 'relative',
+  width: '100%',
+  height: '80vh', // 70% height of viewport
+  overflow: 'hidden',
+  background: 'lightblue', // Set the background color here
+  borderRadius: '10px', // Add rounded corners to the carousel  
+   };
 
  const imageContainerStyle = {
     width: '100%',
@@ -96,13 +93,13 @@ const FelizCumpleaños = () => {
     right: 0,
     padding: '20px',
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'row',
     alignItems: 'flex-end',
  };
 
  const footerStyle = {
-  background: 'linear-gradient(to right, #0000FF, #FFFF00)',
-  color: '#FFFFFF',
+  background: 'linear-gradient(to right, violet, #FFFF00)',
+  color: 'grey',
   textAlign: 'center',
   padding: '20px',
   position: 'fixed',  // Fijar el pie de página a la ventana gráfica
@@ -112,7 +109,7 @@ const FelizCumpleaños = () => {
 };
 
  const buttonStyle = {
-  background: '#4CAF50',
+  background: 'blue',
   border: 'none',
   color: 'white',
   padding: '15px 16px', // Ancho ajustado para ambos botones
@@ -143,8 +140,7 @@ const FelizCumpleaños = () => {
  return (
     <div style={containerStyle}>
       <div style={messageElementStyle}>
-
-        <h1>Feliz Cumpleaños PAPA !!! 06/01/1958</h1>
+        <h2>Feliz Cumpleaños PAPA 06/01/1958</h2>
       </div>
       <div style={carouselStyle}>
         <div style={imageContainerStyle}>
@@ -173,12 +169,11 @@ const FelizCumpleaños = () => {
       <div>
       <>
            <footer style={footerStyle}>
-            <h2>Creada 06/01/2024 por tu hijo Gonzalo Daniel Aguilar</h2>
+            <h3>Creado por tu hijo Gonzalo Daniel Aguilar</h3>
         </footer>
       </>
     </div>
-);
-    </div>
+  </div>
  );
 };
 
